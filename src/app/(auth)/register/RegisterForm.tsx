c33b1d9924d4866bacf665e1c91c9cd84a3c7bd7
Marc Granier <mgranier@comcast.net>
2024-06-01 15:23:32 -0400

    Complete Sigining in Users

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -10,7 +10,8 @@ export default function RegisterForm() {
 	const {
 		register,
 		handleSubmit,
-		formState: { errors, isValid },
+		setError,
+		formState: { errors, isValid, isSubmitting },
 	} = useForm<RegisterSchema>({
 		// resolver: zodResolver(registerSchema),
 		mode: 'onTouched',
@@ -18,7 +19,19 @@ export default function RegisterForm() {
 
 	const onSubmit = async (data: RegisterSchema) => {
 		const result = await registerUser(data);
-		console.log({ result });
+
+		if (result.status === 'success') {
+			console.log('User registered successfully');
+		} else {
+			if (Array.isArray(result.error)) {
+				result.error.forEach((e) => {
+					const fieldName = e.path.join('.') as 'email' | 'name' | 'password';
+					setError(fieldName, { message: e.message });
+				});
+			} else {
+				setError('root.serverError', { message: result.error });
+			}
+		}
 	};
 
 	return (
@@ -60,7 +73,13 @@ export default function RegisterForm() {
 							isInvalid={!!errors.password}
 							errorMessage={errors.password?.message}
 						/>
+						{errors.root?.serverError && (
+							<p className='text-danger text-sm'>
+								{errors.root.serverError.message}
+							</p>
+						)}
 						<Button
+							isLoading={isSubmitting}
 							isDisabled={!isValid}
 							fullWidth
 							color='secondary'
