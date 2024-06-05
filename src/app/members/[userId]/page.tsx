2b29fb29c101719bd041cd27bb6f4493fd28b316
Marc Granier <mgranier@comcast.net>
2024-06-05 14:06:05 -0400

    Complete using dynamic routes to access users

---
+++ src/app/members/[userId]/page.tsx
@@ -0,0 +1,12 @@
+import { getMemberByUserId } from "@/app/actions/memberAction";
+import { notFound } from "next/navigation";
+
+export default async function MemberDetailedPage({params}: {params: {userId: string}}) {
+  const member = await getMemberByUserId(params.userId);
+
+  if (!member) return notFound()
+
+  return (
+    <div>{member.name}</div>
+  )
+}
