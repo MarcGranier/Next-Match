9222d556019bc77dce70f5011fe43c0abab2b030
Marc Granier <mgranier@comcast.net>
2024-06-10 19:03:16 -0400

    Complete adding the buttons for image actions

--- src/app/members/edit/photos/page.tsx
+++ src/app/members/edit/photos/page.tsx
@@ -1,5 +1,7 @@
 import { getAuthUserId } from '@/app/actions/authActions';
 import { getMemberPhotosByUserId } from '@/app/actions/memberAction';
+import DeleteButton from '@/components/DeleteButton';
+import StarButton from '@/components/StarButton';
 import { CardHeader, Divider, CardBody, Image } from '@nextui-org/react';
 
 export default async function PhotosPage() {
@@ -24,6 +26,12 @@ export default async function PhotosPage() {
 									src={photo.url}
 									alt='Image of user'
 								/>
+								<div className='absolute top-3 left-3 z-50'>
+									<StarButton selected={false} loading={false} />
+								</div>
+								<div className='absolute top-3 right-3 z-50'>
+									<DeleteButton loading={false} />
+								</div>
 							</div>
 						))}
 				</div>
