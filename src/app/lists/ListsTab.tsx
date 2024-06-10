cf1830664fd7da40872e40478658156a251e0422
Marc Granier <mgranier@comcast.net>
2024-06-10 13:40:55 -0400

    Complete ListTabs & End Of Section 5

--- src/app/lists/ListsTab.tsx
+++ src/app/lists/ListsTab.tsx
@@ -3,8 +3,9 @@
 import { Tab, Tabs } from '@nextui-org/react';
 import { Member } from '@prisma/client';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { Key } from 'react';
+import { Key, useTransition } from 'react';
 import MemberCard from '../members/MemberCard';
+import LoadingComponent from '@/components/LoadingComponent';
 
 type Props = {
 	members: Member[];
@@ -15,17 +16,20 @@ export default function ListsTab({ members, likeIds }: Props) {
 	const searchParams = useSearchParams();
 	const router = useRouter();
 	const pathname = usePathname();
+	const [isPending, startTransition] = useTransition();
 
 	const tabs = [
 		{ id: 'source', label: 'Members I have liked' },
-		{ id: 'targer', label: 'Members that like me' },
+		{ id: 'target', label: 'Members that like me' },
 		{ id: 'mutual', label: 'Mutual likes' },
 	];
 
 	function handleTabChange(key: Key) {
-		const params = new URLSearchParams(searchParams);
-		params.set('type', key.toString());
-		router.replace(`${pathname}?${params.toString()}`);
+		startTransition(() => {
+			const params = new URLSearchParams(searchParams);
+			params.set('type', key.toString());
+			router.replace(`${pathname}?${params.toString()}`);
+		});
 	}
 
 	return (
@@ -36,23 +40,29 @@ export default function ListsTab({ members, likeIds }: Props) {
 				color='secondary'
 				onSelectionChange={(key) => handleTabChange(key)}
 			>
-				{tabs.map((item) => (
+				{(item) => (
 					<Tab key={item.id} title={item.label}>
-						{members.length > 0 ? (
-							<div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
-								{members.map((member) => (
-									<MemberCard
-										key={member.id}
-										member={member}
-										likeIds={likeIds}
-									/>
-								))}
-							</div>
+						{isPending ? (
+							<LoadingComponent />
 						) : (
-							<div>No memberfor this filter</div>
+							<>
+								{members.length > 0 ? (
+									<div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
+										{members.map((member) => (
+											<MemberCard
+												key={member.id}
+												member={member}
+												likeIds={likeIds}
+											/>
+										))}
+									</div>
+								) : (
+									<div>No members for this filter</div>
+								)}
+							</>
 						)}
 					</Tab>
-				))}
+				)}
 			</Tabs>
 		</div>
 	);
