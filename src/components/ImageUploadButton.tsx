abd72fc7e4aad6e2319bd864252c37c992ad301a
Marc Granier <mgranier@comcast.net>
2024-06-11 18:03:13 -0400

    Starting Upload Images

---
+++ src/components/ImageUploadButton.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { HiPhoto } from 'react-icons/hi2';
+import { CldUploadButton } from 'next-cloudinary';
+
+export default function ImageUploadButton() {
+	return (
+		<CldUploadButton
+			options={{ maxFiles: 1 }}
+			onSuccess={(res) => console.log(res)}
+			signatureEndpoint='/api/sign-image'
+			uploadPreset='nm-demo'
+			className='flex items-center gap-2 bg-secondary text-white rounded-lg py-2 px-4 hover:bg-secondary/70'
+		>
+			<HiPhoto size={28} />
+			Upload new image
+		</CldUploadButton>
+	);
+}
