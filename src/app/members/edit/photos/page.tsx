c697a7ace6fd37a000791f982a2489397c7b8608
Marc Granier <mgranier@comcast.net>
2024-06-13 17:16:26 -0400

    Complete setting the main image

--- src/app/members/edit/photos/page.tsx
+++ src/app/members/edit/photos/page.tsx
@@ -10,6 +10,7 @@ import { CardHeader, Divider, CardBody, Image } from '@nextui-org/react';
 import MemberPhotoUpload from './MemberPhotoUpload';
 import MemberImage from '@/components/MemberImage';
 import ImageUploadButton from '@/components/ImageUploadButton';
+import MemberPhotos from '@/components/MemberPhotos';
 
 export default async function PhotosPage() {
 	const userId = await getAuthUserId();
@@ -24,20 +25,11 @@ export default async function PhotosPage() {
 			<Divider />
 			<CardBody>
 				<MemberPhotoUpload />
-				<div className='grid grid-cols-5 gap-3 p-5'>
-					{photos &&
-						photos?.map((photo) => (
-							<div key={photo.id} className='relative'>
-								<MemberImage photo={photo} />
-								<div className='absolute top-3 left-3 z-50'>
-									<StarButton selected={false} loading={false} />
-								</div>
-								<div className='absolute top-3 right-3 z-50'>
-									<DeleteButton loading={false} />
-								</div>
-							</div>
-						))}
-				</div>
+				<MemberPhotos
+					photos={photos}
+					editing={true}
+					mainImageUrl={member?.image}
+				/>
 			</CardBody>
 		</>
 	);
