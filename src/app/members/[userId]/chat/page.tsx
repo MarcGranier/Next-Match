282aeb999ba1e623baa28fa751e9ca38104393de
Marc Granier <mgranier@comcast.net>
2024-06-24 07:37:26 -0400

    Complete Displaying the messages Part 1

--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -2,20 +2,33 @@ import CardInnerWrapper from '@/components/CardInnerWrapper';
 import ChatForm from './ChatForm';
 import { getMessageThread } from '@/app/actions/messageActions';
 import { getAuthUserId } from '@/app/actions/authActions';
+import MessageBox from './MessageBox';
 
 export default async function ChatPage({
 	params,
 }: {
 	params: { userId: string };
 }) {
-	const userId = await getAuthUserId();
 	const messages = await getMessageThread(params.userId);
-	console.log(messages);
-	return (
-		<CardInnerWrapper
-			header='Chat'
-			body={<div>Chat goes here</div>}
-			footer={<ChatForm />}
-		/>
+	const userId = await getAuthUserId();
+
+	const body = (
+		<div>
+			{messages.length === 0 ? (
+				'No messages to display'
+			) : (
+				<div>
+					{messages.map((message) => (
+						<MessageBox
+							key={message.id}
+							message={message}
+							currentUserId={userId}
+						/>
+					))}
+				</div>
+			)}
+		</div>
 	);
+
+	return <CardInnerWrapper header='Chat' body={body} footer={<ChatForm />} />;
 }
