9b1884935d76be27442f747d57261d24588f1336
Marc Granier <mgranier@comcast.net>
2024-06-24 18:12:53 -0400

    Complete creating the message table

--- src/app/messages/MessageTable.tsx
+++ src/app/messages/MessageTable.tsx
@@ -1,19 +1,27 @@
 'use client';
 
 import { MessageDto } from '@/types';
-import { Table, TableBody, TableColumn, TableHeader } from '@nextui-org/react';
+import {
+	Card,
+	Table,
+	TableBody,
+	TableCell,
+	TableColumn,
+	TableHeader,
+	TableRow,
+	getKeyValue,
+} from '@nextui-org/react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import { Key } from 'react';
 
 type Props = {
 	messages: MessageDto[];
 };
 
 export default function MessageTable({ messages }: Props) {
-	const searchParams = useSearchParams();
 	const router = useRouter();
+	const searchParams = useSearchParams();
 	const isOutbox = searchParams.get('container') === 'outbox';
-	const [isDeleting, setDeleting] = useState({ id: '', loading: false });
 
 	const columns = [
 		{
@@ -22,13 +30,44 @@ export default function MessageTable({ messages }: Props) {
 		},
 		{ key: 'text', label: 'Message' },
 		{ key: 'created', label: isOutbox ? 'Date sent' : 'Date received' },
-		{ key: 'actions', label: 'Actions' },
 	];
 
-	<Table
-		aria-label='Table with messages'
-		selectionMode='single'
-		onRowAction={(key) => handleRowSelect(key)}
-		shadow='none'
-	></Table>;
+	const handleRowSelect = (key: Key) => {
+		const message = messages.find((m) => m.id === key);
+		const url = isOutbox
+			? `/members/${message?.recipientId}`
+			: `/members/${message?.senderId}`;
+		router.push(url + '/chat');
+	};
+
+	return (
+		<Card className='flex flex-col gap-3 h-[80vh] overflow-auto'>
+			<Table
+				aria-label='Table with messages'
+				selectionMode='single'
+				onRowAction={(key) => {
+					handleRowSelect(key);
+				}}
+				shadow='none'
+			>
+				<TableHeader columns={columns}>
+					{(column) => (
+						<TableColumn key={column.key}>{column.label}</TableColumn>
+					)}
+				</TableHeader>
+				<TableBody
+					items={messages}
+					emptyContent='No messages for this container'
+				>
+					{(item) => (
+						<TableRow key={item.id} className='cursor-pointer'>
+							{(columnKey) => (
+								<TableCell>{getKeyValue(item, columnKey)}</TableCell>
+							)}
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+		</Card>
+	);
 }
