647c6f46348a67fda21195c1763fa7cfda2bc0ab
Marc Granier <mgranier@comcast.net>
2024-06-24 12:54:10 -0400

    Complete adding the message sidebar

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,3 +1,12 @@
+import MessageSidebar from './MessageSidebar';
+
 export default function MessagesPage() {
-	return <div>MessagesPage</div>
+	return (
+		<div className='grid grid-cols-12 gap-5 h-[80v] mt-10'>
+			<div className='col-span-2'>
+				<MessageSidebar />
+			</div>
+			<div className='col-span-10'>Message table gores here</div>
+		</div>
+	);
 }
