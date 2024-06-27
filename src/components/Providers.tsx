bedf2fdf02637de18d9a5dd738523bc91fd0c417
Marc Granier <mgranier@comcast.net>
2024-06-27 17:51:02 -0400

    Complete setting up a private channel

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,21 @@
 'use client';
 
 import { usePresenceChannel } from '@/hooks/UsePresenceChannel';
+import { useNotificationChannel } from '@/hooks/useNotificationChannel';
 import { NextUIProvider } from '@nextui-org/react';
 import { ReactNode } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export default function Providers({ children }: { children: ReactNode }) {
+export default function Providers({
+	children,
+	userId,
+}: {
+	children: ReactNode,
+	userId: string | null;
+}) {
 	usePresenceChannel();
+	useNotificationChannel(userId);
 	return (
 		<NextUIProvider>
 			<ToastContainer position='bottom-right' className='z-50' />
