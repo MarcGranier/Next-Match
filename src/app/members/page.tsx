fe131256950b80eec1d866a788583ca0fb19595b
Marc Granier <mgranier@comcast.net>
2024-06-30 16:21:05 -0400

    Spacer32

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,17 +2,23 @@ import Link from 'next/link';
 import { getMembers } from '../actions/memberAction';
 import MemberCard from './MemberCard';
 import { fetchCurrentUserLikeIds } from '../actions/likeActions';
+import PaginationComponent from '@/components/PaginationComponent';
 
-export default async function MembersPage() {
+export default async function MembersPage({
+	searchParams: { searchParams: UserFilters },
+}) {
 	const members = await getMembers();
 	const likeIds = await fetchCurrentUserLikeIds();
 
 	return (
-		<div className='mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
-			{members &&
-				members.map((member) => (
-					<MemberCard member={member} key={member.id} likeIds={likeIds} />
-				))}
-		</div>
+		<>
+			<div className='mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8'>
+				{members &&
+					members.map((member) => (
+						<MemberCard member={member} key={member.id} likeIds={likeIds} />
+					))}
+			</div>
+			<PaginationComponent />
+		</>
 	);
 }
