db26a6bb5499c127f5f3ef5888bb6f10bc532ce8
Marc Granier <mgranier@comcast.net>
2024-07-08 10:19:12 -0400

    Complete adding the sort functionality

--- src/components/navbar/Filters.tsx
+++ src/components/navbar/Filters.tsx
@@ -1,6 +1,12 @@
 'use client';
 
-import { Button, Select, SelectItem, Slider } from '@nextui-org/react';
+import {
+	Button,
+	Select,
+	SelectItem,
+	Slider,
+	Selection,
+} from '@nextui-org/react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React, { useState } from 'react';
 import { FaFemale, FaMale } from 'react-icons/fa';
@@ -25,6 +31,14 @@ export default function Filters() {
 		const params = new URLSearchParams(searchParams);
 		params.set('ageRange', value.join(','));
 		router.replace(`${pathname}?${params}`);
+	};
+
+	const handleOrderSelect = (value: Selection) => {
+		if (value instanceof Set) {
+			const params = new URLSearchParams(searchParams);
+			params.set('orderBy', value.values().next().value);
+			router.replace(`${pathname}?${params}`);
+		}
 	};
 
 	if (pathname !== '/members') return null;
@@ -55,10 +69,12 @@ export default function Filters() {
 					<Select
 						size='sm'
 						fullWidth
-						placeholder='Order by'
+						label='Order by'
 						variant='bordered'
 						color='secondary'
 						aria-label='Order by selector'
+						selectedKeys={new Set([searchParams.get('orderBy') || 'updated'])}
+						onSelectionChange={handleOrderSelect}
 					>
 						{orderByList.map((item) => (
 							<SelectItem key={item.value} value={item.value}>
