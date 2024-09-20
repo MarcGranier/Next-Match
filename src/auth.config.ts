549b1ad3fafc396c638047d5da85ad4f03f6dbe2
Marc Granier <mgranier@comcast.net>
2024-09-20 18:37:20 -0400

    Complete Social Login Part 1

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -9,13 +9,13 @@ import { compare } from 'bcryptjs';
 
 export default {
 	providers: [
-		Google({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		}),
 		Github({
 			clientId: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
+		Google({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 		Credentials({
 			name: 'credentials',
