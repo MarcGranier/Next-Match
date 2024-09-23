0ee76f442589eeac34a3521ecf736974589a15ba
Marc Granier <mgranier@comcast.net>
2024-09-23 14:48:16 -0400

    Complete adding the role to Prisma and the session data

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import authConfig from './auth.config';
 import { prisma } from './lib/prisma';
+import { Role } from '@prisma/client';
 
 export const {
 	handlers: { GET, POST },
@@ -12,13 +13,16 @@ export const {
 	callbacks: {
 		async jwt({ user, token }) {
 			if (user) {
+				console.log({ user });
 				token.profileComplete = user.profileComplete;
+				token.role = user.role;
 			}
 			return token;
 		},
 		async session({ token, session }) {
 			if (token.sub && session.user) session.user.id = token.sub;
 			session.user.profileComplete = token.profileComplete as boolean;
+			session.user.role = token.role as Role;
 			return session;
 		},
 	},
