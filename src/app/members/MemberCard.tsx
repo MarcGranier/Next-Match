11eddd4cc54b7bb998b19fbe4d7c9cc04b7a27c6
Marc Granier <mgranier@comcast.net>
2024-09-29 18:39:10 -0400

    Complete adding a spinner/Complete section 12

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -6,47 +6,70 @@ import { calculateAge, transformImageUrl } from '@/lib/util';
 import { Card, CardFooter, Image } from '@nextui-org/react';
 import { Member } from '@prisma/client';
 import Link from 'next/link';
+import React, { useState } from 'react';
+import { toggleLikeMember } from '../actions/likeActions';
 
 type Props = {
-	member: Member;
-	likeIds: string[];
+    member: Member;
+    likeIds: string[];
 };
 
-function MemberCard({ member, likeIds }: Props) {
-	const hasLiked = likeIds.includes(member.userId);
-	const preventLinkAction = (e: React.MouseEvent) => {
-		e.preventDefault();
-		e.stopPropagation();
-	};
+export default function MemberCard({ member, likeIds }: Props) {
+    const [hasLiked, setHasLiked] = useState(likeIds.includes(member.userId));
+    const [loading, setLoading] = useState(false);
 
-	return (
-		<Card fullWidth as={Link} href={`/members/${member.userId}`} isPressable>
-			<Image
-				isZoomed
-				alt={member.name}
-				width={300}
-				src={transformImageUrl(member.image) || '/images/user.png'}
-				className='aspect-square object-cover'
-			/>
-			<div onClick={preventLinkAction}>
-				<div className='absolute top-3 right-3 z-50'>
-					<LikeButton targetId={member.userId} hasLiked={hasLiked} />
-				</div>
-				<div className='absolute top-2 left-3 z-50'>
-					<PresenceDot member={member} />
-				</div>
-			</div>
-			<CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
-				<div className='flex flex-col text-white'>
-					<span className='font-semibold'>
-						{member.name}, {calculateAge(member.dateOfBirth)}
-					</span>
-					<span className='text-sm'>
-						{member.city}, {member.country}
-					</span>
-				</div>
-			</CardFooter>
-		</Card>
-	);
+    async function toggleLike() {
+        setLoading(true);
+        try {
+            await toggleLikeMember(member.userId, hasLiked);
+            setHasLiked(!hasLiked);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const preventLinkAction = (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+    };
+
+    return (
+        <Card
+            fullWidth
+            as={Link}
+            href={`/members/${member.userId}`}
+            isPressable
+        >
+            <Image
+                isZoomed
+                alt={member.name}
+                width={300}
+                src={transformImageUrl(member.image) || '/images/user.png'}
+                className="aspect-square object-cover"
+            />
+            <div onClick={preventLinkAction}>
+                <div className="absolute top-3 right-3 z-50">
+                    <LikeButton
+                        loading={loading}
+                        toggleLike={toggleLike}
+                        hasLiked={hasLiked}
+                    />
+                </div>
+                <div className="absolute top-2 left-3 z-50">
+                    <PresenceDot member={member} />
+                </div>
+            </div>
+
+            <CardFooter className="flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient">
+                <div className="flex flex-col text-white">
+                    <span className="font-semibold">
+                        {member.name}, {calculateAge(member.dateOfBirth)}
+                    </span>
+                    <span className="text-sm">{member.city}</span>
+                </div>
+            </CardFooter>
+        </Card>
+    );
 }
-export default MemberCard;
