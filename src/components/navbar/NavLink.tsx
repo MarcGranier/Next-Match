3f017e255c1891f12815d0fea300765c382bd7a1
Marc Granier <mgranier@comcast.net>
2024-09-29 16:33:00 -0400

    Complete Updating the Home Page

--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -6,19 +6,21 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 type Props = {
-	href: string;
-	label: string;
+    href: string;
+    label: string;
 };
 
 export default function NavLink({ href, label }: Props) {
-	const pathname = usePathname();
-	const { unreadCount } = useMessageStore((state) => ({
-		unreadCount: state.unreadCount,
-	}));
-	return (
-		<NavbarItem isActive={pathname === href} as={Link} href={href}>
-			<span>{label}</span>
-			{href === '/messages' && <span className='ml-1'>({unreadCount})</span>}
-		</NavbarItem>
-	);
+    const pathname = usePathname();
+    const { unreadCount } = useMessageStore((state) => ({
+        unreadCount: state.unreadCount,
+    }));
+    return (
+        <NavbarItem isActive={pathname === href} as={Link} href={href}>
+            <span>{label}</span>
+            {href === '/messages' && unreadCount > 0 && (
+                <span className="ml-1">({unreadCount})</span>
+            )}
+        </NavbarItem>
+    );
 }
--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,10 +1,10 @@
 import {
-	Button,
-	Navbar,
-	NavbarBrand,
-	NavbarContent,
-	NavbarItem,
-	navbar,
+    Button,
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    navbar,
 } from '@nextui-org/react';
 import Link from 'next/link';
 import { GiMatchTip } from 'react-icons/gi';
@@ -15,71 +15,78 @@ import { getUserInfoForNav } from '@/app/actions/userActions';
 import FiltersWrapper from './FiltersWrapper';
 
 export default async function TopNav() {
-	const session = await auth();
-	const userInfo = session?.user && (await getUserInfoForNav());
+    const session = await auth();
+    const userInfo = session?.user && (await getUserInfoForNav());
 
-	const memberLinks = [
-		{ href: '/members', label: 'Matches' },
-		{ href: '/lists', label: 'Lists' },
-		{ href: '/messages', label: 'Messages' },
-	];
+    const memberLinks = [
+        { href: '/members', label: 'Matches' },
+        { href: '/lists', label: 'Lists' },
+        { href: '/messages', label: 'Messages' },
+    ];
 
-	const adminLinks = [{ href: '/admin/moderation', label: 'Photo Moderation' }];
+    const adminLinks = [
+        { href: '/admin/moderation', label: 'Photo Moderation' },
+    ];
 
-	const links = session?.user.role === 'ADMIN' ? adminLinks : memberLinks;
+    const links = session?.user.role === 'ADMIN' ? adminLinks : memberLinks;
 
-	return (
-		<>
-			<Navbar
-				maxWidth='xl'
-				className='bg-gradient-to-r from bg-purple-400 to-purple-700'
-				classNames={{
-					item: [
-						'text-xl',
-						'text-white',
-						'uppercase',
-						'data-[active=true]:text-yellow-200',
-					],
-				}}
-			>
-				<NavbarBrand as={Link} href='/'>
-					<GiMatchTip size={40} className='text-gray-200' />
-					<div className='font-bold text-3xl flex'>
-						<span className='text-gray-900'>Next</span>
-						<span className='text-gray-200'>Match</span>
-					</div>
-				</NavbarBrand>
-				<NavbarContent justify='center'>
-					{links.map((item) => (
-						<NavLink key={item.href} href={item.href} label={item.label} />
-					))}
-				</NavbarContent>
-				<NavbarContent justify='end'>
-					{userInfo ? (
-						<UserMenu userInfo={userInfo} />
-					) : (
-						<>
-							<Button
-								as={Link}
-								href='/login'
-								variant='bordered'
-								className='text-white'
-							>
-								Login
-							</Button>
-							<Button
-								as={Link}
-								href='/register'
-								variant='bordered'
-								className='text-white'
-							>
-								Register
-							</Button>
-						</>
-					)}
-				</NavbarContent>
-			</Navbar>
-			<FiltersWrapper />
-		</>
-	);
+    return (
+        <>
+            <Navbar
+                maxWidth="xl"
+                className="bg-gradient-to-r from bg-purple-400 to-purple-700"
+                classNames={{
+                    item: [
+                        'text-xl',
+                        'text-white',
+                        'uppercase',
+                        'data-[active=true]:text-yellow-200',
+                    ],
+                }}
+            >
+                <NavbarBrand as={Link} href="/">
+                    <GiMatchTip size={40} className="text-gray-200" />
+                    <div className="font-bold text-3xl flex">
+                        <span className="text-gray-900">Next</span>
+                        <span className="text-gray-200">Match</span>
+                    </div>
+                </NavbarBrand>
+                <NavbarContent justify="center">
+                    {session &&
+                        links.map((item) => (
+                            <NavLink
+                                key={item.href}
+                                href={item.href}
+                                label={item.label}
+                            />
+                        ))}
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    {userInfo ? (
+                        <UserMenu userInfo={userInfo} />
+                    ) : (
+                        <>
+                            <Button
+                                as={Link}
+                                href="/login"
+                                variant="bordered"
+                                className="text-white"
+                            >
+                                Login
+                            </Button>
+                            <Button
+                                as={Link}
+                                href="/register"
+                                variant="bordered"
+                                className="text-white"
+                            >
+                                Register
+                            </Button>
+                        </>
+                    )}
+                </NavbarContent>
+            </Navbar>
+            <FiltersWrapper />
+        </>
+    );
 }
