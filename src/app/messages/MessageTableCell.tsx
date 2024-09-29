8bb811ef5369688538299dc04866864d8dba6b5e
Marc Granier <mgranier@comcast.net>
2024-09-29 15:18:07 -0400

    Complete adding a confirmation modal

--- src/app/messages/MessageTableCell.tsx
+++ src/app/messages/MessageTableCell.tsx
@@ -6,63 +6,76 @@ import { AiFillDelete } from 'react-icons/ai';
 import AppModal from '@/components/AppModal';
 
 type Props = {
-	item: MessageDto;
-	columnKey: string;
-	isOutbox: boolean;
-	deleteMessage: (message: MessageDto) => void;
-	isDeleting: boolean;
+    item: MessageDto;
+    columnKey: string;
+    isOutbox: boolean;
+    deleteMessage: (message: MessageDto) => void;
+    isDeleting: boolean;
 };
 
 export default function MessageTableCell({
-	item,
-	columnKey,
-	isOutbox,
-	deleteMessage,
-	isDeleting,
+    item,
+    columnKey,
+    isOutbox,
+    deleteMessage,
+    isDeleting,
 }: Props) {
-	const cellValue = item[columnKey as keyof MessageDto];
-	const { isOpen, onOpen, onClose } = useDisclosure();
+    const cellValue = item[columnKey as keyof MessageDto];
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-	const footerButtons: ButtonProps[] = [
-		{ color: 'default', onClick: onClose, children: 'Close' },
-		{ color: 'secondary', onClick: onClose, children: 'Submit' },
-	];
+    const onConfirmDeleteMessage = () => {
+        deleteMessage(item);
+    };
 
-	switch (columnKey) {
-		case 'recipientName':
-		case 'senderName':
-			return (
-				<div className='flex items-center gap-2 cursor-pointer '>
-					<PresenceAvatar
-						userId={isOutbox ? item.recipientId : item.senderId}
-						src={isOutbox ? item.recipientImage : item.senderImage}
-					/>
-					<span>{cellValue}</span>
-				</div>
-			);
-		case 'text':
-			return <div>{truncateString(cellValue, 80)}</div>;
-		case 'created':
-			return <div>{cellValue}</div>;
-		default:
-			return (
-				<>
-					<Button
-						isIconOnly
-						variant='light'
-						onClick={() => onOpen()}
-						isLoading={isDeleting}
-					>
-						<AiFillDelete size={24} className='text-danger' />
-					</Button>
-					<AppModal
-						isOpen={isOpen}
-						onClose={onClose}
-						header='Test Modal'
-						body={<div>Just Testing</div>}
-						footerButtons={footerButtons}
-					/>
-				</>
-			);
-	}
+    const footerButtons: ButtonProps[] = [
+        { color: 'default', onClick: onClose, children: 'Cancel' },
+        {
+            color: 'secondary',
+            onClick: onConfirmDeleteMessage,
+            children: 'Confirm',
+        },
+    ];
+
+    switch (columnKey) {
+        case 'recipientName':
+        case 'senderName':
+            return (
+                <div className="flex items-center gap-2 cursor-pointer ">
+                    <PresenceAvatar
+                        userId={isOutbox ? item.recipientId : item.senderId}
+                        src={isOutbox ? item.recipientImage : item.senderImage}
+                    />
+                    <span>{cellValue}</span>
+                </div>
+            );
+        case 'text':
+            return <div>{truncateString(cellValue, 80)}</div>;
+        case 'created':
+            return <div>{cellValue}</div>;
+        default:
+            return (
+                <>
+                    <Button
+                        isIconOnly
+                        variant="light"
+                        onClick={() => onOpen()}
+                        isLoading={isDeleting}
+                    >
+                        <AiFillDelete size={24} className="text-danger" />
+                    </Button>
+                    <AppModal
+                        isOpen={isOpen}
+                        onClose={onClose}
+                        header="Please confirm this action"
+                        body={
+                            <div>
+                                Are you sure you want to delete this message?
+                                This cannot be undone.
+                            </div>
+                        }
+                        footerButtons={footerButtons}
+                    />
+                </>
+            );
+    }
 }
--- src/components/AppModal.tsx
+++ src/components/AppModal.tsx
@@ -1,43 +1,59 @@
 import {
-	Button,
-	ButtonProps,
-	Modal,
-	ModalBody,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
+    Button,
+    ButtonProps,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
 } from '@nextui-org/react';
 import { ReactNode } from 'react';
 
 type Props = {
-	isOpen: boolean;
-	onClose: () => void;
-	header: string;
-	body: ReactNode;
-	footerButtons: ButtonProps[];
+    isOpen: boolean;
+    onClose: () => void;
+    header: string;
+    body: ReactNode;
+    footerButtons: ButtonProps[];
 };
 
 export default function AppModal({
-	isOpen,
-	onClose,
-	header,
-	body,
-	footerButtons,
+    isOpen,
+    onClose,
+    header,
+    body,
+    footerButtons,
 }: Props) {
-	return (
-		<Modal isOpen={isOpen} onClose={onClose} placement='top-center'>
-			<ModalContent>
-				<ModalHeader className='flex flex-col gap-1'>{header}</ModalHeader>
-				<ModalBody>{body}</ModalBody>
-				<ModalFooter>
-					{footerButtons &&
-						footerButtons.map((props: ButtonProps, index) => (
-							<Button {...props} key={index}>
-								{props.children}
-							</Button>
-						))}
-				</ModalFooter>
-			</ModalContent>
-		</Modal>
-	);
+    return (
+        <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+            placement="top-center"
+            motionProps={{
+                variants: {
+                    enter: {
+                        y: 0,
+                        opacity: 100,
+                        transition: { duration: 0.3 },
+                    },
+                    exit: { y: 100, opacity: 0, transition: { duration: 0.3 } },
+                },
+            }}
+        >
+            <ModalContent>
+                <ModalHeader className="flex flex-col gap-1">
+                    {header}
+                </ModalHeader>
+                <ModalBody>{body}</ModalBody>
+                <ModalFooter>
+                    {footerButtons &&
+                        footerButtons.map((props: ButtonProps, index) => (
+                            <Button {...props} key={index}>
+                                {props.children}
+                            </Button>
+                        ))}
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    );
 }
